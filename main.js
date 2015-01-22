ffb571914e764759e78f7d45f393a3533d56e52c
Tyler Cecchi <thcecchi@gmail.com>
2015-01-22 09:38:50 -0500

    fifth commit

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ var $photo4 = $(".photoFrame").eq(3);
 var $photo5 = $(".photoFrame").eq(4);
 var $photo6 = $(".photoFrame").eq(5);
 
+// BACK BUTTON VARIABLE
+var $back = $(".backButton");
+
 // SELECT ALBUM
 $album1.click(function(event) {
   $(".albumWrapper").removeClass("active");
@@ -66,123 +69,113 @@ $photo1.click(function(event) {
   $("aside").removeClass("active");
   $("section").removeClass("collapsed");
   $("section").addClass("fullscreen");
-  $(".photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(4),.photoFrame:eq(5)").remove();
+  $(".photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(4),.photoFrame:eq(5)").hide();
 });
 
 $photo2.click(function(event) {
   $("aside").removeClass("active");
   $("section").removeClass("collapsed");
   $("section").addClass("fullscreen");
-  $(".photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(4),.photoFrame:eq(5)").remove();
+  $(".photoFrame:eq(0),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(4),.photoFrame:eq(5)").hide();
 });
 
 $photo3.click(function(event) {
   $("aside").removeClass("active");
   $("section").removeClass("collapsed");
   $("section").addClass("fullscreen");
-  $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(3),.photoFrame:eq(4),.photoFrame:eq(5)").remove();
+  $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(3),.photoFrame:eq(4),.photoFrame:eq(5)").hide();
 });
 
 $photo4.click(function(event) {
   $("aside").removeClass("active");
   $("section").removeClass("collapsed");
   $("section").addClass("fullscreen");
-  $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(4),.photoFrame:eq(5)").remove();
+  $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(4),.photoFrame:eq(5)").hide();
 });
 
 $photo5.click(function(event) {
   $("aside").removeClass("active");
   $("section").removeClass("collapsed");
   $("section").addClass("fullscreen");
-  $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(5)").remove();
+  $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(5)").hide();
 });
 
 $photo6.click(function(event) {
   $("aside").removeClass("active");
   $("section").removeClass("collapsed");
   $("section").addClass("fullscreen");
-  $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(5)").remove();
+  $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(5)").hide();
 });
 
+//NAV MENU
 
+  var $Nav1 = $("nav a").eq(0);
+  var $Nav2 = $("nav a").eq(1);
+  var $Nav3 = $("nav a").eq(2);
+  var $Nav4 = $("nav a").eq(3);
+  var $Nav5 = $("nav a").eq(4);
+  var $Nav6 = $("nav a").eq(5);
 
-
-
-
-
-
-
-
-
-
-
-  //NAV MENU
-
-  var $album1Nav = $("nav a").eq(0);
-  var $album2Nav = $("nav a").eq(1);
-  var $album3Nav = $("nav a").eq(2);
-  var $album4Nav = $("nav a").eq(3);
-  var $album5Nav = $("nav a").eq(4);
-  var $album6Nav = $("nav a").eq(5);
-
-  $album1Nav.click(function(event) {
+  $Nav1.click(function(event) {
     event.preventDefault();
 
+    $(".photoWrapper1").addClass("active");
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
-
-    // $(".post1").siblings().removeClass("active");
-    // $(".post1").addClass("active");
+    $(".postWrapper1").siblings().removeClass("active");
   });
 
-  $album2Nav.click(function(event) {
+  $Nav2.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
-
-    // $('.post2').siblings().removeClass("active");
-    // $('.post2').addClass("active");
+    $(".photoWrapper2").addClass("active");
+    $('.photoWrapper2').siblings().removeClass("active");
   });
 
-  $album3Nav.click(function(event) {
+  $Nav3.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
-
-    // $(".post3").siblings().removeClass("active");
-    // $(".post3").addClass("active");
+    $(".photoWrapper3").siblings().removeClass("active");
+    $(".photoWrapper3").addClass("active");
   });
 
-  $album4Nav.click(function(event) {
+  $Nav4.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
-
-    // $(".post1").siblings().removeClass("active");
-    // $(".post1").addClass("active");
+    $(".photoWrapper4").siblings().removeClass("active");
+    $(".photoWrapper4").addClass("active");
   });
 
-  $album5Nav.click(function(event) {
+  $Nav5.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
-
-    // $('.post2').siblings().removeClass("active");
-    // $('.post2').addClass("active");
+    $(".photoWrapper5").addClass("active");
+    $('.photoWrapper5').siblings().removeClass("active");
   });
 
-  $album6Nav.click(function(event) {
+  $Nav6.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
+    $(".postWrapper6").siblings().removeClass("active");
+    $(".photoWrapper6").addClass("active");
+  });
 
-    // $(".post3").siblings().removeClass("active");
-    // $(".post3").addClass("active");
+// Back Button
+  $back.click(function(event) {
+    $("aside").addClass("active");
+    $("section").removeClass("fullscreen");
+    $("section").addClass("collapsed");
+    $(".photoFrame:eq(0),.photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(4),.photoFrame:eq(5)").show();
   });
 
 });
