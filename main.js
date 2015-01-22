edae1cc72bb6f722d37b0ad92ce5b6cc3a14b8bd
Tyler Cecchi <thcecchi@gmail.com>
2015-01-21 19:59:13 -0500

    third commit

--- main.js
+++ main.js
@@ -1,74 +1,117 @@
 $(document).ready(function() {
   "use strict"; // Execute our JS safely when DOM is fully loaded
 
+// ALBUM FRAME VARIABLES
+var $album1 = $(".albumFrame").eq(0);
+var $album2 = $(".albumFrame").eq(1);
+var $album3 = $(".albumFrame").eq(2);
+var $album4 = $(".albumFrame").eq(3);
+var $album5 = $(".albumFrame").eq(4);
+var $album6 = $(".albumFrame").eq(5);
+
+//PHOTO FRAME VARIABLES
+var $photo1 = $(".photoFrame").eq(0);
+var $photo2 = $(".photoFrame").eq(1);
+var $photo3 = $(".photoFrame").eq(2);
+var $photo4 = $(".photoFrame").eq(3);
+var $photo5 = $(".photoFrame").eq(4);
+var $photo6 = $(".photoFrame").eq(5);
+
+// SELECT ALBUM
+$album1.click(function(event) {
+  $(".albumWrapper").removeClass("active");
+  $(".photoWrapper").addClass("active");
+  $("aside").addClass("active");
+  $("section").addClass("collapsed");
+});
+
+//SELECT PHOTO
+$photo1.click(function(event) {
+  $("aside").removeClass("active");
+  $("section").removeClass("collapsed");
+  $("section").addClass("fullscreen");
+  $(".photoFrame:eq(1),.photoFrame:eq(2),.photoFrame:eq(3),.photoFrame:eq(4),.photoFrame:eq(5)").remove();
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
   //NAV MENU
 
-  var $album1 = $("nav a").eq(0);
-  var $album2 = $("nav a").eq(1);
-  var $album3 = $("nav a").eq(2);
-  var $album4 = $("nav a").eq(3);
-  var $album5 = $("nav a").eq(4);
-  var $album6 = $("nav a").eq(5);
+  var $album1Nav = $("nav a").eq(0);
+  var $album2Nav = $("nav a").eq(1);
+  var $album3Nav = $("nav a").eq(2);
+  var $album4Nav = $("nav a").eq(3);
+  var $album5Nav = $("nav a").eq(4);
+  var $album6Nav = $("nav a").eq(5);
 
-  $album1.click(function(event) {
+  $album1Nav.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
 
-    $(".post1").siblings().removeClass("active");
-    $(".post1").addClass("active");
+    // $(".post1").siblings().removeClass("active");
+    // $(".post1").addClass("active");
   });
 
-  $album2.click(function(event) {
+  $album2Nav.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
 
-    $('.post2').siblings().removeClass("active");
-    $('.post2').addClass("active");
+    // $('.post2').siblings().removeClass("active");
+    // $('.post2').addClass("active");
   });
 
-  $album3.click(function(event) {
+  $album3Nav.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
 
-    $(".post3").siblings().removeClass("active");
-    $(".post3").addClass("active");
+    // $(".post3").siblings().removeClass("active");
+    // $(".post3").addClass("active");
   });
 
-  $album4.click(function(event) {
+  $album4Nav.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
 
-    $(".post1").siblings().removeClass("active");
-    $(".post1").addClass("active");
+    // $(".post1").siblings().removeClass("active");
+    // $(".post1").addClass("active");
   });
 
-  $album5.click(function(event) {
+  $album5Nav.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
 
-    $('.post2').siblings().removeClass("active");
-    $('.post2').addClass("active");
+    // $('.post2').siblings().removeClass("active");
+    // $('.post2').addClass("active");
   });
 
-  $album6.click(function(event) {
+  $album6Nav.click(function(event) {
     event.preventDefault();
 
     $(this).closest("li").siblings().removeClass("activeNav");
     $(this).closest("li").addClass("activeNav");
 
-    $(".post3").siblings().removeClass("active");
-    $(".post3").addClass("active");
+    // $(".post3").siblings().removeClass("active");
+    // $(".post3").addClass("active");
   });
 
 });
